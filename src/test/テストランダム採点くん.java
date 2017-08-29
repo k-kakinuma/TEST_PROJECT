2d18ede05281c463d35d9dc9f4498de29824b5a9
k-kakinuma <bohemian521@gmail.com>
2017-08-29 13:55:18 +0900

    TEST COMMIT2

--- src/test/テストランダム採点くん.java
+++ src/test/テストランダム採点くん.java
@@ -9,8 +9,8 @@ public class テストランダム採点くん {
 		// Randomクラス
 		Random random = new Random();
 
-		// 0から100までランダムで数字を生成する
-		int num = random.nextInt(101);
+		// 0から1000までランダムで数字を生成する
+		int num = random.nextInt(1001);
 
 		// 点数を表示する
 		System.out.println("あなたの点数は" + num + "点です。");
